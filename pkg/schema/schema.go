0de589a0280b213b9339d8cd29ffcc8f7461ef26
dvncan <dunbrown64@gmail.com>
2026-03-25 16:13:28 -0400

    146/RemoveBranchable

--- pkg/schema/schema.go
+++ pkg/schema/schema.go
@@ -5,40 +5,17 @@ import (
 	"strings"
 )
 
-//go:embed schema_standard.graphql
+//go:embed schema.graphql
 var SchemaGraphQL string
-
-//go:embed schema_branchable.graphql
-var SchemaBranchGraphQL string
 
 // GetSchema returns the GraphQL schema found in `schema.graphql` as a string.
 func GetSchema() string {
 	return SchemaGraphQL
 }
 
-// GetBranchableSchema returns the branchable GraphQL schema.
-func GetBranchableSchema() string {
-	return SchemaBranchGraphQL
-}
-
-// GetSchemaForBuild returns the appropriate schema based on build tags.
-func GetSchemaForBuild() string {
-	return schemaForBuild(IsBranchable())
-}
-
-func schemaForBuild(branchable bool) string {
-	if branchable {
-		return GetBranchableSchema()
-	}
-	return GetSchema()
-}
-
 // GetSchemaForChain returns the schema with collection names adapted for the given chain prefix.
 // It replaces the default "Ethereum__Mainnet" prefix with the provided one.
 func GetSchemaForChain(prefix string) string {
-	s := GetSchemaForBuild()
-	if prefix == "" || prefix == "Ethereum__Mainnet" {
-		return s
-	}
+	s := GetSchema();
 	return strings.ReplaceAll(s, "Ethereum__Mainnet", prefix)
 }
