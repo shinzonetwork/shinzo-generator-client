e3919791a6a1329266e328446e6853aedf79e470
johnnymatthews <9611008+johnnymatthews@users.noreply.github.com>
2026-05-07 09:43:53 +0100

    Checks for `docker` and `docker compose`.
    Alright, a couple of things here. There was a bug where the script would
    fail if Docker was already installed, so added a check for that. I also
    replaced `docker-composez standalone with the new (and prefered) `docker
    compose` plugin, so everything now matches the official Docker install
    instructions.

--- indexer-prod.sh
+++ indexer-prod.sh
@@ -1,5 +1,5 @@
-# Sets up SSL certificates, installs Docker, and starts the indexer via 
-# docker-compose. Run this after indexer-prod-setup.sh has been executed.
+# Sets up SSL certificates, installs Docker, and starts the indexer via
+# docker compose. Run this after indexer-prod-setup.sh has been executed.
 
 set -e &&
 sudo mkdir -p ~/ssl &&
@@ -8,8 +8,10 @@ sudo openssl req -new -key ~/ssl/nginx.key -out /tmp/nginx.csr -subj "/C=US/ST=S
 sudo openssl x509 -req -days 365 -in /tmp/nginx.csr -signkey ~/ssl/nginx.key -out ~/ssl/nginx.crt &&
 sudo rm /tmp/nginx.csr
 
-sudo apt-get update &&
-sudo apt-get install -y docker.io docker-compose &&
+if ! command -v docker >/dev/null 2>&1 || ! docker compose version >/dev/null 2>&1; then
+  sudo apt-get update &&
+  sudo apt-get install -y docker.io docker-compose-v2
+fi &&
 sudo mkdir -p ~/data/defradb ~/data/lens &&
 sudo chown -R 0:0 ~/data/defradb ~/data/lens &&
-docker-compose up -d
+docker compose up -d
